b476d2bb0d7522c4e5d55fe0202d7c741d7a25ad
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-05-22 10:40:25 +0200

    Adding Values to the array

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
-const buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
-  return randomNumber;
+  var randomChosenColour = buttonColours[randomNumber];
+
+  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  gamePattern.push(randomChosenColour);
 }
