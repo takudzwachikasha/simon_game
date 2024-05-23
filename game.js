56802ba735c56cd366891d461844edb49ea3b304
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-05-23 21:40:39 +0200

    changes

--- game.js
+++ game.js
@@ -11,6 +11,6 @@ var randomChosenColour = buttonColours[nextSequence()];
 gamePattern.push(randomChosenColour);
 var $element = $("#" + randomChosenColour);
 
-
+//Test ths
 $element.animate({ opacity: 0.1 }, 500)
         .animate({ opacity: 1 }, 500);
