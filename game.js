db21b4915ae1cae77c0e2670aafa490ace9cec4d
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-05-23 21:39:31 +0200

    Changes

--- game.js
+++ game.js
@@ -11,6 +11,6 @@ var randomChosenColour = buttonColours[nextSequence()];
 gamePattern.push(randomChosenColour);
 var $element = $("#" + randomChosenColour);
 
-// Now you can perform operations on the selected elemen
+
 $element.animate({ opacity: 0.1 }, 500)
         .animate({ opacity: 1 }, 500);
